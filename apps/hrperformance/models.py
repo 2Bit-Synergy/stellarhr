689ec680fa3ff7e0caa7f6b4cf2ff575c3e232d8
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2024-01-30 23:44:27 +0800

    Added hrattendance app

---
+++ apps/hrperformance/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from apps.employees.models import Employee
+
+# Create your models here.
+
+
+
+# class AttendanceRecord(models.Model):
+#     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+#     time_in = models.DateTimeField(models.BigAutoField(_("")))
