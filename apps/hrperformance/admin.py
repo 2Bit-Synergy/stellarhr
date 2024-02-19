7185afd042fa51852877095344d920eb073bfe92
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2024-02-20 04:59:08 +0800

    Added models on admin

--- apps/hrperformance/admin.py
+++ apps/hrperformance/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from apps.hrperformance.models import AttendanceRecord, TimeLogType, HRSetting
+from apps.hrperformance.models import AttendanceRecord, TimeLogType, HRSetting, Recognition, Offense
 
 
 
 admin.site.register(TimeLogType)
 admin.site.register(AttendanceRecord)
 admin.site.register(HRSetting)
+admin.site.register(Recognition)
+admin.site.register(Offense)
+
