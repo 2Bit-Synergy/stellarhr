8ce08750b8d017f494b0fa1c72533c59777f90db
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2024-02-25 14:29:49 +0800

    Started CRUD on Performance

--- apps/hrperformance/urls.py
+++ apps/hrperformance/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
-from apps.hrperformance.views import AttendanceRecordView, HRSettingView
+from apps.hrperformance.views import AttendanceRecordView, HRSettingUpdateView, OffenseCreateView
+from . import views
 
 urlpatterns = [
     # path('my-dashboard', AttendanceRecordView.as_view(), name="my_dashboard")
     path('my-attendance', AttendanceRecordView.as_view(), name="my_dashboard"),
-    path('hr-settings', HRSettingView.as_view(), name="hr_settings")
+    path('hr-settings/', HRSettingUpdateView.as_view(), name="hr_settings"),
+    path('temporary-success-url/', views.temporary_success_url, name='temporary_sucess_url'),
+    path('log-offense/<int:id>/', OffenseCreateView.as_view(), name="log_offense")
 ]
